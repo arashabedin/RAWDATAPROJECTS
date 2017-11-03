7dd8aa2839da4c83bf5cde4dda9e47f0c1b1e781
arashabedin <arash.abedin@gmail.com>
2017-11-03 15:24:13 +0100

    merged questions controllers

--- SOVAapp/WebService/Controllers/QuestionController.cs
+++ SOVAapp/WebService/Controllers/QuestionController.cs
@@ -13,7 +13,7 @@ using Microsoft.AspNetCore.Routing;
 
 namespace WebService.Controllers
 {
-    [Route("api/question/{id}")]
+    [Route("api/questions")]
     public class QuestionController : CustomeController
     { 
         private readonly IRepository _repository;
@@ -27,20 +27,61 @@ namespace WebService.Controllers
 
     }
 
-    [HttpGet(Name = nameof(GetQuestionById))]
-    public IActionResult GetQuestionById(int Id)
+        [HttpGet(Name = nameof(GetQuestions))]
+        public IActionResult GetQuestions(int page = 0, int pageSize = 2)
+        {
+            CheckPageSize(ref pageSize);
+
+            var total = _repository.CountQuestions();
+            var totalPages = GetTotalPages(pageSize, total);
+
+            var data = _repository.GetQuestions(page, pageSize)
+                .Select(x => new QuestionModel
+                {
+                    Url = Url.Link(nameof(QuestionController.GetQuestionById), new { Qid = x.Id }),
+                    UserName = x.UserInfo.OwnerUserDisplayName,
+                    CreationDate = x.CreationDate,
+                    Score = x.Score,
+                    Title = x.Title,
+                    Body = x.Body,
+                    UserUrl = Url.Link(nameof(UserController.GetUserByUserId), new { id = x.OwnerUserId }),
+                    // AcceptedAnswerUrl = Url.Link(nameof(UserController.GetUserByUserId), new { id = x.OwnerUserId }),
+                    AnswersUrl = Url.Link(nameof(QuestionAnswersController.GetAnswersByQuestionId), new { id = x.Id }),
+                    CommentsUrl = Url.Link(nameof(CommentController.GetCommentsByQuestionId), new { Qid = x.Id })
+
+                });
+
+            var result = new
+            {
+                Total = total,
+                Pages = totalPages,
+                Page = page,
+                Prev = Link(nameof(GetQuestions), page, pageSize, -1, () => page > 0),
+                Next = Link(nameof(GetQuestions), page, pageSize, 1, () => page < totalPages - 1),
+                Url = Link(nameof(GetQuestions), page, pageSize),
+                Data = data
+            };
+
+            return Ok(result);
+        }
+
+
+
+
+        [HttpGet("{Qid}", Name = nameof(GetQuestionById))]
+    public IActionResult GetQuestionById(int Qid)
     {
 
-        var Question = _repository.GetQuestionById(Id);
+        var Question = _repository.GetQuestionById(Qid);
         if (Question == null) return NotFound();
 
         var model = _mapper.Map<QuestionModel>(Question);
-            model.UserName = Question.UserInfo.DisplayName;
-        model.Url = Url.Link(nameof(GetQuestionById), new { id = Question.Id });
+        model.UserName = Question.UserInfo.DisplayName;
+        model.Url = Url.Link(nameof(GetQuestionById), new { Qid = Question.Id });
         model.UserUrl = Url.Link(nameof(UserController.GetUserByUserId), new { id = Question.OwneruserId });
         model.AcceptedAnswerUrl = Url.Link(nameof(AnswerController.GetAnswerById), new { id = Question.AcceptedAnswerId });
         model.AnswersUrl = Url.Link(nameof(QuestionAnswersController.GetAnswersByQuestionId), new { id = Question.Id });
-            model.CommentsUrl = Url.Link(nameof(CommentController.GetCommentsByQuestionId), new { Qid = Question.Id }); 
+        model.CommentsUrl = Url.Link(nameof(CommentController.GetCommentsByQuestionId), new { Qid = Question.Id }); 
         return Ok(model);
     }
 
