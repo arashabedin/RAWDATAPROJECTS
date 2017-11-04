39db5db52ddca8b8784dcf118e407c341839efff
kaur2605 <32565739+kaur2605@users.noreply.github.com>
2017-11-04 07:22:40 +0100

    Installed Moq and added WebServiceTests...

---
+++ SOVAapp/UnitTests/WebServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using Xunit;
+using DataService.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTests
+{
+    class WebServiceTest
+    {
+
+        private const string questionsApi = "http://localhost:5001/api/question";
+
+        [Fact]
+        public void GetQuestions_ValidId_ReturnsOk()
+        {
+            var mockDataAccess = new Mock<IRepository>();
+            mockDataAccess.Setup(o => o.GetQuestionById(It.IsAny<int>())).Returns(new Question());
+
+            var QuestionsControllers = new QuestionControllers(mockDataAccess.Object);
+
+            var response = QuestionsControllers.GetQuestionById(1);
+
+            Assert.IsType<OkObjectResult>(response);
+
+        }
+
+        [Fact]
+        public void GetQuestionById_InvalidId_ReturnsNotFund()
+        {
+            var dataServviceMock = new Mock<IRepository>();
+
+            var ctrl = new QuestionsControllers(dataServviceMock.Object);
+
+            var response = ctrl.GetQuestionById(2);
+
+            Assert.IsType<NotFoundResult>(response);
+        }
+
+    }
+}
