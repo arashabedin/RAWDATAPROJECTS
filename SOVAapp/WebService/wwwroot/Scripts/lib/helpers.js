1c92c8d03be73a1d9b0e82e1a2e3fd8dd7ee0252
arashabedin <arash.abedin@gmail.com>
2017-12-11 11:42:54 +0100

    Fixed some collapsing functions

--- SOVAapp/WebService/wwwroot/Scripts/lib/helpers.js
+++ SOVAapp/WebService/wwwroot/Scripts/lib/helpers.js
@@ -7,6 +7,6 @@
 var collapseComments = function(){
 $('.accordion-group').on('click.collapse-next.data-api', '[data-toggle=collapse-next]', function () {
     var $target = $(this).parent().next()
-    $target.data('collapse') ? $target.collapse('toggle') : $target.collapse()
+    $target.data('collapse') ? $target.collapse('toggle') : $target.collapse('toggle')
     });
 }
