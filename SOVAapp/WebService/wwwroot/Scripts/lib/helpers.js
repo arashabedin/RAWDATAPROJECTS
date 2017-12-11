615a7f4cbb3392e153acb898cc440c65d5587985
arashabedin <arash.abedin@gmail.com>
2017-12-11 11:45:11 +0100

    //commenting

--- SOVAapp/WebService/wwwroot/Scripts/lib/helpers.js
+++ SOVAapp/WebService/wwwroot/Scripts/lib/helpers.js
@@ -1,9 +1,11 @@
-﻿var loadingHint = function () {
+﻿//Loading Hint or Searching
+var loadingHint = function () {
     if ($('#mySearchData').is(':empty')) {
         $("#loading").text("Please wait until the content is loaded. It might take time for the results to emerged, but we promise you intresting results...");
     }
 }
 
+//Comments Collapsing
 var collapseComments = function(){
 $('.accordion-group').on('click.collapse-next.data-api', '[data-toggle=collapse-next]', function () {
     var $target = $(this).parent().next()
