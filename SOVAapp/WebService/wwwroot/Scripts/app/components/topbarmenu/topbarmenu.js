759e3f076e359d0fd7b6b96c0920b312d4dcb6ca
arashabedin <arash.abedin@gmail.com>
2017-12-23 23:30:20 +0100

    some small changes

--- SOVAapp/WebService/wwwroot/Scripts/app/components/topbarmenu/topbarmenu.js
+++ SOVAapp/WebService/wwwroot/Scripts/app/components/topbarmenu/topbarmenu.js
@@ -18,46 +18,15 @@
 
         changeContent(config.defaultMenu);
 
-        var searchBarContents = ko.observable("");
+       
 
-        postman.subscribe(function (value) {
-            searchBarContents(value);
-        }, "searchBarContent","topBarContext");
-        var searchContentLength = ko.computed(function () {
-            if (searchBarContents().length === 1) {
-                $("#functionalSearchBar").slideDown("fast", function () {
 
-                    // If the window is wider than 768px, set the functional search bar to be half the length
-                    // of the primary search bar. If it's not, our bootstrap handles the fluid transistion, 
-                    // so override css width styling.
-                    if ($(window).width() > 768) {
-                        $("#functionalSearchBar").css('width',($("#primarySearchBar").width() / 2) + 'px');
-                    } else $("#functionalSearchBar").css('width', '');
-
-                    $("#functionalSearchBar").show();
-                    $("#functionalSearchBar").focus();
-                });
-            } else if (searchBarContents().length > 0) {
-                $("#functionalSearchBar").show();
-                $("#functionalSearchBar").focus();
-            } else {
-                $("#functionalSearchBar").slideUp("fast", function () {
-                    $("#functionalSearchBar").hide();
-                }); 
-            }
-
-            postman.notify(searchBarContents(), "searchBarContent");
-
-            return searchBarContents().length;
-        });
 
         return {
             menuElements: config.menuElements,
             currentComponent: currentComponent,
             changeContent: changeContent,
             isMenuSelected: isMenuSelected,
-            searchBarContents: searchBarContents,
-            searchContentLength: searchContentLength,
             applicationName: applicationName,
             defaultComponent: config.defaultMenuItem
         }
